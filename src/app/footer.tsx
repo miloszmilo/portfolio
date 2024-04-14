eb512a0b3015d30e09b015d405f4d93e06c73546
NxtPerfect <alakaxan@protonmail.com>
2024-04-14 20:17:33 +0200

    More work on home page
    main hero now takes whole page
    styled project cards better
    minor work on footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <>Footer</>
+  return <footer className="flex flex-row w-full min-h-24 justify-center items-center bg-neutral-700">
+    <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/'}>Home</Link>
+    <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/about'}>About me</Link>
+    <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/contact'}>Contact</Link>
+    <span>Created using Nextjs, tailwind, supabase.</span>
+  </footer>
 }
