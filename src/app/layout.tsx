1a0584c70d6384f5d21d3afc1636b3c37b8847da
NxtPerfect <alakaxan@protonmail.com>
2024-04-14 20:17:33 +0200

    More work on home page
    main hero now takes whole page
    styled project cards better
    minor work on footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <Navbar />
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
 export default function Nav() {
-  return <nav className="w-100 grid grid-cols-2 grid-rows-1 px-4 justify-between align-middle">
+  return <nav className="sticky top-0 w-full grid grid-cols-2 grid-rows-1 px-4 py-2 justify-between align-middle bg-black/50 backdrop-blur-md">
     <div className="w-100 flex justify-self-center items-center">
       <p className="py-1 font-mono">To strive for the better is to be humane</p>
     </div>
     <div className="w-100 flex flex-row justify-center items-center gap-4">
-      <Link className="px-2 py-1" href={'/'}>Home</Link>
-      <Link className="px-2 py-1" href={'/about'}>About me</Link>
-      <Link className="rounded-xl bg-yellow-400 text-gray-700 hover:bg-yellow-600 hover:text-gray-100 active:bg-yellow-300 duration-150 transition-all text-lg font-semibold px-5 py-1" href={'/contact'}>Contact</Link>
+      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/'}>Home</Link>
+      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/about'}>About me</Link>
+      <Link className="rounded-xl bg-yellow-400 text-gray-700 hover:bg-yellow-300 active:text-gray-100 active:bg-yellow-500 duration-150 transition-all text-lg font-semibold px-5 py-1" href={'/contact'}>Contact</Link>
     </div>
   </nav>
 }
