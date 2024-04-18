31ba359ed5f92983e39db61451e5915963e6c7a3
NxtPerfect <alakaxan@protonmail.com>
2024-04-18 13:30:37 +0200

    Working on links next to scroll indicator
    currently projects are shown at 40% height, instead of 75%

--- src/app/scroll.tsx
+++ src/app/scroll.tsx
@@ -8,20 +8,23 @@ export default function Scroll() {
       let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
       let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       setScroll(((winScroll / height) * 100).toFixed(2));
-      let scrollProg = document.getElementById("progress")
     }
+    let projectLink = document.querySelector("#project-link")
+    projectLink.style.top = (document.querySelector('#projects').getBoundingClientRect().top / document.querySelector("footer").getBoundingClientRect().bottom) * 50.0 + 25.0 + "svh" // Return from 25 to 75 svh
+
     document.addEventListener("scroll", onScroll)
     return () => {
       document.removeEventListener("scroll", onScroll)
     }
   }, [])
+
   return (
     <>
       <div className="fixed left-[95%] top-[25svh] h-[50svh] w-1 bg-white/50 overflow-hidden z-50">
         <div id="progress" className="bg-white w-full h-full transition duration-150 ease-in" style={{ transform: `translateY(${scroll - 100}%)` }}></div>
       </div>
       <a href="#top" className="fixed left-[90%] top-[25svh] hover:text-sky-500 transition duration-150">The Best</a>
-      <a href="#projects" className="fixed left-[90%] top-[73svh] hover:text-sky-500 transition duration-150">Projects</a>
+      <a id="project-link" href="#projects" className="fixed left-[90%] hover:text-sky-500 transition duration-150">Projects</a>
     </>
   )
 }
