d088d8b332bd0f69cae8de37df6b41f8b832b0ec
NxtPerfect <alakaxan@protonmail.com>
2024-04-22 20:05:03 +0200

    Added mailto button
    added name on home page
    changed footer
    added contact button on the bottom
    modified scrollbar to align text to right

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,9 +6,10 @@ export default function Nav() {
       <p className="py-1 font-mono">To strive for the better is to be humane</p>
     </div>
     <div className="w-100 flex flex-row justify-center items-center gap-4">
-      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/'}>Home</Link>
-      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'/about'}>About me</Link>
-      <Link className="rounded-xl bg-yellow-400 text-gray-700 hover:bg-yellow-300 active:text-gray-100 active:bg-yellow-500 duration-150 transition-all text-lg font-semibold px-5 py-1" href={'/contact'}>Contact</Link>
+      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'#top'}>Home</Link>
+      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'#projects'}>Projects</Link>
+      <Link className="px-2 py-1 hover:text-sky-500 hover:underline transition duration-150" href={'#resume'}>Resume</Link>
+      <a href="mailto:mail@tutanota.com" className="rounded-xl bg-yellow-400 text-gray-700 hover:bg-yellow-300 active:text-gray-100 active:bg-yellow-500 duration-150 transition-all text-lg font-semibold px-5 py-1">Contact me</a>
     </div>
   </nav>
 }
--- src/app/scroll.tsx
+++ src/app/scroll.tsx
@@ -23,12 +23,12 @@ export default function Scroll() {
 
   return (
     <>
-      <div className="fixed left-[95%] top-[25svh] h-[50svh] w-1 bg-white/50 overflow-hidden z-50">
+      <div className="fixed right-[5%] top-[25svh] h-[50svh] w-1 bg-white/50 overflow-hidden z-50">
         <div id="progress" className="bg-yellow-500 w-full h-full transition duration-150 ease-in" style={{ transform: `translateY(${scroll - 100}%)` }}></div>
       </div>
-      <a href="#top" className="fixed left-[90%] top-[25svh] hover:text-sky-500 transition duration-150">The Best</a>
-      <a id="project-link" href="#projects" className="fixed left-[90%] hover:text-sky-500 transition duration-150">Projects</a>
-      <a id="resume-link" href="#resume" className="fixed left-[90%] hover:text-sky-500 transition duration-150">Resume</a>
+      <a href="#top" className="fixed right-[6%] top-[25svh] hover:text-sky-500 transition duration-150">Home</a>
+      <a id="project-link" href="#projects" className="fixed right-[6%] hover:text-sky-500 transition duration-150">Projects</a>
+      <a id="resume-link" href="#resume" className="fixed right-[6%] hover:text-sky-500 transition duration-150">Resume</a>
     </>
   )
 }
