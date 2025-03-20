6b1e051847824f97c5f3c22219dcc205c1430de4
NxtPerfect <alakaxan@protonmail.com>
2025-03-20 11:19:02 +0100

    fix: removed image from project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,12 @@
-import Image from 'next/image'
 import React from 'react'
 import LinkTo from './LinkTo'
 
 type Props = {
   name: string
   description: string
-  imageUrl: string
 }
 
-export default function Project({ name, description, imageUrl }: Props) {
-  // <Image src={imageUrl} alt="image of project" fill />
+export default function Project({ name, description }: Props) {
   return (
     <div className={`flex flex-col text-left min-h-16 w-[20ch]`}>
       <div className={`p-2 bg-background dark:bg-primary text-primary
