b800f9d72b0bca43156b2d8091e25c24044cc493
NxtPerfect <alakaxan@protonmail.com>
2025-03-21 08:23:08 +0100

    feat: added cta button
    feat: reworked spacing around sections
    feat: added projects section
    feat: removed unnecessary public files

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import LinkTo from './LinkTo'
+import Image from 'next/image'
 
 type Props = {
   name: string
   description: string
+  imageUrl: string
 }
 
-export default function Project({ name, description }: Props) {
+export default function Project({ name, description, imageUrl }: Props) {
   return (
-    <div className={`flex flex-col text-left min-h-16 w-[20ch]`}>
-      <div className={`p-2 bg-background dark:bg-primary text-primary
-dark:text-background w-[20ch] h-[4lh] overflow-ellipsis`}>
-        <p className="text-[1.5rem]">
+    <div className={`flex flex-col text-left h-[18lh] w-[40ch]`}>
+      <div className="h-3/5 w-full overflow-clip">
+        <Image src={imageUrl} alt="Project preview" width={400} height={400} />
+      </div>
+      <div className={`px-4 py-2 bg-background dark:bg-primary text-primary
+dark:text-background w-full h-2/5 overflow-ellipsis`}>
+        <p className="text-p">
           <LinkTo href={`https://github.com/NxtPerfect/{name}`}>
             {name}
           </LinkTo>
         </p>
-        <p className="text-pretty">
+        <p className="text-pretty text-small-p overflow-ellipsis">
           {description}
         </p>
       </div>
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -11,22 +11,22 @@ export default function ProjectList() {
   const projects: Array<ProjectData> = [
     {
       name: "Flaminger",
-      description: "job search platform without biases",
-      imageUrl: ""
+      description: "job search platform without biases, where you’ll always know reason behind your rejection",
+      imageUrl: "/websitePlaceholder.jpg"
     },
     {
       name: "GemUI",
-      description: "accessible react components",
-      imageUrl: ""
+      description: "react component library optimized for maximum accessibility without compromising usability",
+      imageUrl: "/websitePlaceholder.jpg"
     },
     {
       name: "EasyVim",
-      description: "vim motions gamified",
-      imageUrl: ""
+      description: "practice vim motions in gamified environment, earn rewards, defeat bosses and become more powerful",
+      imageUrl: "/websitePlaceholder.jpg"
     }
   ]
   return (
-    <div className="flex flex-row justify-center gap-32">
+    <div className="flex flex-row justify-evenly gap-36">
       {projects.map((project, index) => {
         return (
           <Project key={index} {...project} />
