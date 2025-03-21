dcf26bcdbfc29ad711ca2d70bece1884fa0cf305
NxtPerfect <alakaxan@protonmail.com>
2025-03-21 10:25:35 +0100

    feat: added support for dark theme
    feat: timeline now properly shows line breaks

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,18 +1,20 @@
-"use client";
 import React from 'react'
 
 type Props = {
   children: React.ReactNode
-  onClickAction: () => void
 }
 
-export default function CTAButton({ children, onClickAction }: Props) {
+export default function CTAButton({ children }: Props) {
+  function scrollToElement() {
+    const contact = document.getElementById("contact");
+    contact?.scrollIntoView({ behavior: "smooth" });
+  }
   return (
     <button type="button"
       className={`italic bg-primary border-1 border-background w-fit
 p-1 px-16 text-h2 mt-28 cursor-pointer text-center align-middle
-hover:bg-accent/50 focus:bg-accent/50`}
-      onClick={onClickAction}>
+hover:bg-accent/50 focus:bg-accent/50 text-background`}
+      onClick={scrollToElement}>
       {children}
     </button>
   )
